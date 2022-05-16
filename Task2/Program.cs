631deecf812da30d24e8774bf9041736e783d038
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-16 23:23:51 +0300

    decided task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найдите сумму всех четных элементов ряда Фибоначчи, которые не превышают четыре миллиона.
+Console.Clear();
+long sumEvenFibo = 0;
+int  secondFibo  = 1;
+int  firstFibo   = 2;
+int  currentFibo = -1;
+
+while(currentFibo < 4000000)
+{
+    currentFibo = firstFibo + secondFibo;
+    secondFibo  = firstFibo;
+    firstFibo   = currentFibo;
+
+    if(currentFibo % 2 == 0) sumEvenFibo += currentFibo;
+}
+Console.WriteLine($"Сумма всех четных чисел ряда фибоначча до 4 000 000 равна {sumEvenFibo}");
