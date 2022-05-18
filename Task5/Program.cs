ee8e0bc433de60f843f2761525684817cdf682d4
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-18 14:09:03 +0300

    add tasks 5

---
+++ Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿//Какое самое маленькое число делится нацело на все числа от 1 до 20?
+double divNum = 20;
+bool   found  = false;
+while (true)
+{
+    for (int i = 1; i <= 20; i++)
+    {
+        if(divNum % i != 0) break;
+        else if(i == 20) found = true;
+    }
+    if(found) break;
+    divNum++;
+}
+
+System.Console.WriteLine(divNum);
+System.Console.WriteLine();
+
+for (int j = 1; j <= 20; j++)
+{
+    System.Console.WriteLine($"{divNum} / {j} = {divNum/j}");
+}
+
